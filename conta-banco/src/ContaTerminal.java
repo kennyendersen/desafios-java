bb4e559d68b8e38eee2570d5d89bcac3a22e6b85
kenny <kennyfreitas00@gmail.com>
2025-02-18 18:32:35 -0400

    trocando \n por 'println'

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -6,17 +6,17 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.print("Olá, Por favor digite o numero da conta: \n");
+        System.out.println("Olá, Por favor digite o numero da conta: ");
         int conta = scanner.nextInt();
         
-        System.out.print("Sua agência: \n");
+        System.out.println("Sua agência: ");
         String agencia = scanner.next();
 
 
-        System.out.print("Seu nome: \n");
+        System.out.println("Seu nome: ");
         String nome = scanner.next();
 
-        System.out.print("Digite seu saldo: \n");
+        System.out.println("Digite seu saldo: ");
         double saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
